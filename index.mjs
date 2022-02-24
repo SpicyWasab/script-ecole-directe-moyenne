59687c01a4e36856584a62529d1f00457b051b31
Wasab <whitenoise09@free.fr>
2022-02-24 20:26:28 +0100

    Patched comments and variables name. It's now fully in english, (et plus avec un franglais bancal xD). Code is now probably a little bit more readable.

--- index.mjs
+++ index.mjs
@@ -8,12 +8,15 @@ import ora from 'ora';
 const username = await prompt('Identifiant : ');
 const password = await prompt.password('Mot de passe : ');
 
+
 // login to ecoledirecte
+// spinner
 let loginSpinner = ora({
     color: 'blue', // ecoledirecte is blue
     text: 'Connexion à EcoleDirecte ...'
 }).start();
 
+// request
 const loginRes = await fetch('https://api.ecoledirecte.com/v3/login.awp', {
     method: 'post',
     body: parseBody({
@@ -24,24 +27,27 @@ const loginRes = await fetch('https://api.ecoledirecte.com/v3/login.awp', {
     headers: getHeader()
 });
 
-// get token, message and datas
-const { token, message, data } = await loginRes.json();
+// get code, token, message and datas
+const { code: loginResCode, token, message, data } = await loginRes.json();
 
-// if no token (fail) -> panic with message
-if(!token) panic(message, loginSpinner);
+// code != 200 (fail) -> panic with message
+if(loginCode != 200) panic(message, loginSpinner);
 
 // otherwise, get the firstname, lastname and id
-const { nom, prenom, id } = data.accounts[0];
+const { prenom: firstName, nom: lastName, id } = data.accounts[0];
 
 // succeed the spinner
-loginSpinner.succeed(`Connecté à EcoleDirecte - ${nom} ${prenom}`);
+loginSpinner.succeed(`Connecté à EcoleDirecte - ${lastName} ${firstName}`);
+
 
 // fetch notes
+// spinner
 const notesSpinner = ora({
     color: 'blue', // ecoledirecte is still blue :)
     text: 'Récupération des notes ...'
 }).start();
 
+// request
 const notesRes = await fetch(`https://api.ecoledirecte.com/v3/eleves/${id}/notes.awp?verbe=get`, {
     method: 'post',
     body: parseBody({
@@ -51,104 +57,106 @@ const notesRes = await fetch(`https://api.ecoledirecte.com/v3/eleves/${id}/notes
 });
 
 // get the code, msg, and notes
-const { code, msg, data: { notes } } = await notesRes.json();
+const { code: notesResCode, msg, data: { notes } } = await notesRes.json();
 
 // if code is not 200 (success) panic
-if(code != 200) panic(msg, notesSpinner);
+if(notesResCode != 200) panic(msg, notesSpinner);
 
+// succeed the spinner
 notesSpinner.succeed('Les notes ont été récupérées avec succès');
 
-// notes / matieres
-let notesParMatieres = { };
-
-// available periods
+// available periods (halfs, terms ...)
 const availablePeriods = [...(new Set(notes.map(note => note.codePeriode)))];
 
 // display available periods
 console.table(availablePeriods);
 
 // ask for the period index (default is the last period, which is the current period (I think :thinking:))
-const periodIndex = await safePromptNumber('Index de la période (semestre, trimestres, etc ...) dont vous voulez obtenir les moyennes :',
-    availablePeriods.length - 1,
-    (value) => parseInt(index) < 0 || parseInt(index) >= availablePeriods.length
-);
+const currentPeriodIndex = availablePeriods.length - 1;
+const verify = (value) => parseInt(index) < 0 || parseInt(index) >= availablePeriods.length;
+const periodIndex = await safePromptNumber('Veuillez entrer l\'index de la période (semestre, trimestres, etc ...) dont vous voulez obtenir les moyennes.', currentPeriodIndex, verify);
 
-const period = availablePeriods[periodIndex];
+// get the selected period code
+const selectedPeriod = availablePeriods[periodIndex];
 
 // ask for over (note/over) (default is 20)
 let over = await safePromptNumber('Sur combien voulez-vous obtenir les moyennes ?', 20, (value) => !isNaN(value));
 
 // ask for precision (digits after point) (default is 2)
-let precision = await safePromptNumber('Veuillez entrer une précision (nombre de chiffres après la virgule) :', 1, (value) => !isNaN(value));
+let precision = await safePromptNumber('Veuillez entrer une précision (nombre de chiffres après la virgule).', 1, (value) => !isNaN(value));
+
+// notes calculated and sorted by subjects
+let subjectsNotes = { };
 
 // for each note
 for(const noteDatas of notes) {
     // get the matiere, period, noteOver, value, coef, isANumberOrALetter (eg: 'Abs' -> absent)
-    const { libelleMatiere, codePeriode, noteSur, valeur, coef, enLettre } = noteDatas;
+    const { libelleMatiere: subject, codePeriode: notePeriod, noteSur: noteOver, valeur: value, coef, enLettre: isLetter } = noteDatas;
 
     // if not selected period, skip
-    if(codePeriode != period) continue;
-    // if not already in notes / matieres, add an entry to notes / matieres
-    if(!(libelleMatiere in notesParMatieres)) notesParMatieres[libelleMatiere] = [ ];
+    if(notePeriod != selectedPeriod) continue;
+    // if the subject isn't already registered in subjectNotes, add an entry to subjectNotes for this subject
+    if(!(subject in subjectsNotes)) subjectsNotes[subject] = [ ];
     // if not numeric (eg: 'Abs'), skip
-    if(enLettre) continue;
+    if(isLetter) continue;
     
     // a simple function to transform '17,4' type number to actual float
     const getFloat = (numberString) => parseFloat(numberString.replace(',', '.'));
 
     // push the matiere entry
-    notesParMatieres[libelleMatiere].push({
-        value: getFloat(valeur) / getFloat(noteSur), // value
+    subjectsNotes[subject].push({
+        value: getFloat(value) / getFloat(noteOver), // note value
         coef: getFloat(coef) // coef
     });
 }
 
-// the object that will be displayed to the end user
-let displayedMoyennes = { };
+// the object that will be displayed to the end user through console.table()
+let displayedAverages = { };
 
 // moyenne generale
-let moyenneGenerale = 0;
-// i (we're assuming every matiere is coef 1)
+let overallAverage = 0;
+// i (we're assuming every matiere is coef 1, so overallAverage will be divided by i)
 let i = 0;
 
-// for every matiere entry (notes)
-for(const matiere in notesParMatieres) {
-    const notes = notesParMatieres[matiere];
+// for every subject entry (notes of that subject)
+for(const subject in subjectsNotes) {
+    const notes = subjectsNotes[subject];
 
-    // if no note, moyenne is undefined
-    if(notesParMatieres.length === 0) displayedMoyennes[matiere] = undefined;
+    // if no note, average is undefined
+    if(subjectsNotes.length === 0) subjectsNotes[subject] = undefined;
     
     // sum of notes, sum of coefs
-    let sommeNotes = 0;
-    let sommeCoefs = 0;
+    let sumOfNotes = 0;
+    let sumOfCoefs = 0;
 
     // for every note
     for(const note of notes) {
-        sommeNotes += note.value * note.coef; // sum notes
-        sommeCoefs += note.coef; // sum coef
+        sumOfNotes += note.value * note.coef; // sum of notes
+        sumOfCoefs += note.coef; // sum of coef
     }
 
-    // calculate the moyenne
-    const moyenne = sommeNotes / sommeCoefs * over;
+    // calculate the average
+    const average = sumOfNotes / sumOfCoefs * over;
     
     displayedMoyennes[matiere] = moyenne.toFixed(precision);
     moyenneGenerale += moyenne;
     i++;
 }
 
-// sort the moyennes from the best to the worst :
-displayedMoyennes = Object.fromEntries((Object.entries(displayedMoyennes)).sort((a, b) => {
+// sort the averages from the best to the worst :
+displayedAverages = Object.fromEntries((Object.entries(displayedAverages)).sort((a, b) => {
     return parseFloat(b[1]) - parseFloat(a[1]);
 }));
 
-// calculate moyenne generale
-moyenneGenerale /= i;
-displayedMoyennes['MOYENNE GENERALE'] = moyenneGenerale.toFixed(precision);
+// calculate overall average
+overallAverage /= i;
+displayedAverages['MOYENNE GENERALE'] = overallAverage.toFixed(precision);
 
-// display all moyennes
-console.table(displayedMoyennes);
+// (finally !) display averages
+console.table(displayedAverages);
 
-// successfully terminate the process
+// successfully terminate the process, because, for whatever reason the process doesn't terminate itself.
+// I think it could be due to prompt-promise. I will probably write my own "input-password" function later.
 process.exit(0);
 
 
@@ -168,6 +176,10 @@ function panic(text, spinner) {
     process.exit(1);
 }
 
+/**
+ * Because EcoleDirecte doesn't like node-fetch's default user-agent (wich is probably "node-fetch") :)
+ * @returns {Object};
+ */
 function getHeader() {
     return { 'User-Agent': "Bonjour EcoleDirecte. Je veux juste calculer ma moyenne en utilisant node.js, mais apparemment l'user agent 'node\\-fetch' (celui par défaut en utilisant le module 'node\\-fetch') ne vous convient pas (c'est d'ailleurs pour ça que je mets un antislash en plein milieu, sinon ça ne fonctionne pas ...). Permettez-moi donc d'utiliser autre chose. (Bonne journée à vous si quelqu'un lit ce message :))" };
 }
